314e3279529012ab93bbcdaeb5451c237cb4bfd2
vinnypirolli <vnp2025@nyu.edu>
2022-04-06 16:39:17 -0400

    hello

---
+++ jsresponse/response.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("small");
+    if (x.innerHTML === "essage") {
+      x.innerHTML = "edium";
+    } else {
+      x.innerHTML = "essage";
+    }
+  }
+ 
